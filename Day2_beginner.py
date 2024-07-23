e0e8b7b13505bfb9e24a99a29677217905f2e81a
Saru <heo_dark@naver.com>
2024-07-23 17:40:18 +0900

    [Day2] 2024-07-23(Tue)

---
+++ Day2_beginner.py
@@ -0,0 +1,8 @@
+# 99club Day2_beginner - 2024-07-23(Tue)
+# https://school.programmers.co.kr/learn/courses/30/lessons/12944
+
+def solution(arr):
+    return sum(arr) / len(arr)
+
+
+print(solution([1,2,3,4]))
---
+++ Day2_middler.py
@@ -0,0 +1,8 @@
+# 99club Day2_beginner - 2024-07-23(Tue)
+# https://school.programmers.co.kr/learn/courses/30/lessons/12954
+
+def solution(x, n):
+    return [x * j for j in range(1, n+1)]
+
+
+print(solution(2, 5))
