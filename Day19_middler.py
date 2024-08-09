ff484484fd97e5e4733d211f601db4dde9f67a28
Saru <heo_dark@naver.com>
2024-08-09 21:33:31 +0900

    [Day19] 2024-08-09(Fri)

---
+++ Day19_middler.py
@@ -0,0 +1,15 @@
+# 99club Day18_middler - 2024-08-09(Fri)
+# https://school.programmers.co.kr/learn/courses/30/lessons/42885
+
+
+def solution(people, limit):
+    people.sort()
+    left, right = 0, len(people) - 1
+    answer = 0
+    while left <= right:
+        if people[left] + people[right] <= limit:
+            left += 1
+        right -= 1
+        answer += 1
+
+    return answer
