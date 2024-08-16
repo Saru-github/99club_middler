fa880a950be1bc4835c58b4a6c1e1e9eb8ad1fa0
Saru <heo_dark@naver.com>
2024-08-16 22:24:41 +0900

    [Day26] 2024-08-16(Fri)

---
+++ Day26_middler.py
@@ -0,0 +1,13 @@
+# 99club Day26_middler - 2024-08-16(Fri)
+# https://school.programmers.co.kr/learn/courses/30/lessons/178871
+
+def solution(players, callings):
+    playerDic = {player: rank for rank, player in enumerate(players)}
+
+    for calling in callings:
+        pre, index = playerDic[calling] - 1, playerDic[calling]  # 앞사람 인덱스, 불린사람 인덱스
+        players[pre], players[index] = players[index], players[pre]  # 배열 순서 바꾸기
+        playerDic[players[pre]] -= 1  # 딕셔너리 값 변경  # 배열 바뀐것 주의
+        playerDic[players[index]] += 1
+
+    return players
